1faf9163518f8b1d0e3af83d7114b5a56aef52cc
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-03-16 05:49:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+#..........This Bot Made By [RAHAT](https://t.me/r4h4t_69)..........#
+#..........Anyone Can Modify This As He Likes..........#
+#..........Just one requests do not remove my credit..........#
+
+import os
+API_ID = 20604892
+API_HASH = "a75d4dab1a2483a157d93e3ae9bf7500"
+BOT_TOKEN = "8174269081:AAEe4vVT9RDTJ6VKh8qkcjjauQiBGkshnWY"
+#Logs
+LOG_CHANNEL = "-1002176525471" 
+# MongoDB setup
+MONGO_URL = "mongodb+srv://nitinkumardhundhara:DARKXSIDE78@cluster0.wdive.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "AutoRenamer"
+
+DOWNLOAD_DIR = "./downloads"
+#ADMIN INFO
+ADMIN = 1136967391
+
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg").split()
